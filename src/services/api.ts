6c883e5f7d5652f9834383b702fb4b6319a11436
Monique <moniquesantosmota900@gmail.com>
2021-10-30 19:20:33 -0300

    Adicionando variáveis de ambiente

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
  
 const api = axios.create({
-    baseURL: 'https://tarefa-backendd.herokuapp.com'
+    baseURL: process.env.REACT_APP_BACKEND
 })
  
 export default api;
